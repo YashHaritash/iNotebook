1b12b44037dce8ca5789a66b465f81af45fb7538
YashHaritash <yashharitash4@gmail.com>
2025-01-01 17:00:13 +0530

    building deleteNote Function without api yet

--- src/components/Noteitem.jsx
+++ src/components/Noteitem.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
 
 function NoteItem(props) {
+  const context = useContext(noteContext);
+  const { deleteNote } = context;
+
   const { note } = props;
   return (
     <div className="col-md-3 my-3">
@@ -8,7 +12,12 @@ function NoteItem(props) {
         <div className="card-body">
           <div className="d-flex">
             <h5 className="card-title">{note.title}</h5>{" "}
-            <i className="fa-solid fa-trash mx-2"></i>
+            <i
+              className="fa-solid fa-trash mx-2"
+              onClick={() => {
+                deleteNote(note._id);
+              }}
+            ></i>
             <i className="fa-solid fa-pen-to-square mx-2"></i>
           </div>
 
--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -113,7 +113,13 @@ const NoteState = (props) => {
     setNotes(notes.concat(note));
   };
   // Delete a note
-  const deleteNote = (id) => {};
+  const deleteNote = (id) => {
+    console.log("Deleting the node with id" + id);
+    const newNotes = notes.filter((note) => {
+      return note._id !== id;
+    });
+    setNotes(newNotes);
+  };
   // Edit a note
   const editNote = (id) => {};
 
